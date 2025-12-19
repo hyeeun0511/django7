4643385ed0f0bd1f9430870ab7911f5972b0689e
hyeeun0511 <bang48188@naver.com>
2025-12-19 18:34:08 +0900

    1219 댓수정

--- d1218/sm_pjt/board/views.py
+++ d1218/sm_pjt/board/views.py
@@ -4,10 +4,19 @@ from comment.models import Comment
 from member.models import Member
 from django.db.models import F,Q
 from django.core.paginator import Paginator
+import requests
+import json
+from . import func_api
 
 # 차트 그리기
 def chart(request):
     return render(request,'board/chart.html')
+
+# 공공데이터api
+def list2(request):
+    public_key = func_api.public_api()
+    context = {'result':'성공'}
+    return render(request,'board/list2.html')
 
 # 게시판 답글달기
 def reply(request,bno):
@@ -29,15 +38,13 @@ def reply(request,bno):
         # 1. 답글달기 : 우선 같은 그룹에 있는 게시글의 bstep 1씩 먼저 증가
         board_qs = Board.objects.filter(bgroup=bgroup,bstep__gt = bstep)
         board_qs.update(bstep=F('bstep')+1) # F함수 : 검색된 그 컬럼에만 값을 적용
-        
+
         # 2. 답글저장
         Board.objects.create(btitle=btitle,bcontent=bcontent,member=member,bgroup=bgroup,\
             bstep=bstep+1,bindent=bindent+1,bfile=bfile)
-        
-        
+
         context = {'flag':1}
         return render(request,'board/reply.html',context)
-
 
 # 게시판 수정
 def update(request,bno):
@@ -57,9 +64,9 @@ def update(request,bno):
         qs.btitle = btitle
         qs.bcontent = bcontent
         if bfile: qs.bfile = bfile
-        qs.save()    
-        return redirect(f'/board/view/{bno}/')    
-                
+        qs.save()
+        return redirect(f'/board/view/{bno}/')
+
 # 게시판 삭제
 def delete(request,bno):
     # 게시글 가져오기
@@ -68,41 +75,51 @@ def delete(request,bno):
     context = {'flag':2}
     return redirect("/board/list/")
 
-# 게시판 상세보기 - 해당 하단댓글도 함께 가져올 수 있음.
+# 게시판 상세보기 - 해당 하단댓글도 함께 가져올수 있음.
 def view(request,bno):
     # 게시글 가져오기
+    page = request.GET.get('page')
+    search = request.GET.get('search')
     qs = Board.objects.filter(bno=bno)
     # 하단댓글
-    c_qs = Comment.objects.filter(board=qs[0])
     # 조회수 1증가
     # 조회를 한후 조회된 데이터들을 update,delete : F
-    qs.update(bhit = F('bhit') + 1 )
-    context = {'board':qs[0],'clist':c_qs}
+    qs.update(bhit = F('bhit') + 1)
+    context = {'board':qs[0],'search':search,'page':page}
     return render(request,'board/view.html',context)
 
-# 게시판 상세보기 - 해당 하단댓글도 함께 가져올 수 있음.
+# 게시판 상세보기 - 해당 하단댓글도 함께 가져올수 있음.
 def view2(request,bno):
-    print('bno :',bno)
+    print("bno: ",bno)
     # 게시글 가져오기
     qs = Board.objects.filter(bno=bno)
     # 하단댓글
-    c_qs = Comment.objects.filter(board=qs[0]).order_by('-cno')
-    context = {'board':qs[0],'clist':c_qs}
+    context = {'board':qs[0]}
     return render(request,'board/view2.html',context)
-
-
 
 # 게시판 리스트
 def list(request):
-    # 게시글 모두 가져오기
-    qs = Board.objects.all().order_by('-bgroup','bstep')
-    # 하단 넘버링 (qs,10) -> 1페이지 10개씩
-    paginator = Paginator(qs,10)  # 101 -> 11
-    # 현재페이지 넘김.
+    search = request.GET.get('search','')
     page = int(request.GET.get('page',1))
-    list_qs = paginator.get_page(page) # 1page -> 게시글 10개를 전달
-    
-    context = {'list':list_qs,'page':page}
+    if search == '':
+        # 게시글 모두 가져오기
+        qs = Board.objects.all().order_by('-bgroup','bstep')
+        # 하단 넘버링 (qs,10) -> 1페이지 10개씩
+        paginator = Paginator(qs,10)  # 101 -> 11
+        # 현재페이지 넘김.
+        list_qs = paginator.get_page(page) # 1page -> 게시글 10개를 전달
+    else:
+        qs = Board.objects.filter(btitle__icontains = search)
+        # and 조건
+        # qs = Board.objects.filter(btitle__icontains = '답글', bcontent__icontations = '답글')
+        # or 조건
+        # qs = Board.objects.filter(Q(btitle__icontains = '답글')|Q(bcontent__icontains = '답글'))
+        # 하단 넘버링 (qs,10) -> 1페이지 10개씩
+        paginator = Paginator(qs,10)  # 101 -> 11
+        # 현재페이지 넘김.
+        list_qs = paginator.get_page(page) # 1page -> 게시글 10개를 전달
+
+    context = {'list':list_qs,'page':page,'search':search}
     return render(request,'board/list.html',context)
 
 # 게시판 글쓰기
@@ -121,4 +138,3 @@ def write(request):
         qs.save()
         context = {'flag':'1'}
         return render(request,'board/write.html',context)
-        
